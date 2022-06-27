8b3b928c1cad5698083c74c693dbaa8e08fa8282
Gabriel Salazar <gsaalcantara@gmail.com>
2022-06-27 03:43:07 -0300

    Dropout adicionado

--- breast_cancer_cruzada.py
+++ breast_cancer_cruzada.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 
@@ -13,9 +13,10 @@ def criarRede():
     classificador = Sequential()
     classificador.add(Dense(units = 16, activation = 'relu',
                             kernel_initializer = 'random_uniform', input_dim = 30))
+    classificador.add(Dropout(0.2))
     classificador.add(Dense(units = 16, activation = 'relu',
                             kernel_initializer = 'random_uniform'))
-    
+    classificador.add(Dropout(0.2))
     classificador.add(Dense(units = 1, activation = 'sigmoid'))
     
     otimizador = keras.optimizers.Adam(lr = 0.001, decay= 0.0001, clipvalue = 0.5)
