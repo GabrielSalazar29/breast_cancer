3a9558ff7e3136c3cfe4c5663feef9ba3d296c5a
Gabriel Salazar <gsaalcantara@gmail.com>
2022-06-27 01:48:54 -0300

    Estrutura da rede neural criada

--- breast_cancer_simples.py
+++ breast_cancer_simples.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
 
 previsores = pd.read_csv('entradas_breast.csv')
 classe = pd.read_csv('saidas_breast.csv')
@@ -7,3 +10,7 @@ classe = pd.read_csv('saidas_breast.csv')
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
 
 
+classificador = Sequential()
+classificador.add(Dense(units = 16, activation = 'relu',
+                        kernel_initializer = 'random_uniform', input_dim = 30))
+classificador.add(Dense(units = 1, activation = 'sigmoid'))
